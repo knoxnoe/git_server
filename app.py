9a487d0f2a079b200cdf9d09abf19807b6086c87
abigaier <853369479@qq.com>
2021-03-15 15:50:36 +0800

    token init

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+# 导入user模块
+from handler.user import user
+# app = Flask(__name__)
+from db_config import app
+# 用户模块 user/login user/register user/changepwd
+app.register_blueprint(user, url_prefix="/user")
 
-app = Flask(__name__)
+
+# 商品模块
+
+# 订单模块
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    return "hello"
 
 
 if __name__ == '__main__':
