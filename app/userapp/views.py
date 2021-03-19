58975fdd2e738f52d49930be22f386c82e0cc042
abigaier <853369479@qq.com>
2021-03-19 17:23:53 +0800

    使用表单获取注册数据

--- app/userapp/views.py
+++ app/userapp/views.py
@@ -22,8 +22,8 @@ def login():
 @user.route('/register', methods=['POST'])
 def reg():
     # get_data = json.loads(request.get_data(as_text=True))
-    nickname = request.json.get('nickname')
-    password_hash = request.json.get('password_hash')
+    nickname = request.form.get('nickname')
+    password_hash = request.form.get('password_hash')
     # 使用用户的注册功能
     result = User_reg(nickname, password_hash)
     return jsonify(result)
