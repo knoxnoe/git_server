2d5759951cb9ca91f02f0d4770fbee94fc61fca5
abigaier <853369479@qq.com>
2021-03-23 20:03:56 +0800

    仓库连通文件系统

--- app/repositoryapp/api.py
+++ app/repositoryapp/api.py
@@ -1,9 +1,10 @@
 import os
+import shutil
 from app.utils import class2data, create_response
 from app.repositoryapp.models import Repository
 from git import Repo
 
-GIT_ROOT = '/root/gitroot/'
+GIT_ROOT = '/gitrepo'
 
 # 创建仓库 api
 def create_repo(nickname, reponame, desc):
@@ -16,7 +17,7 @@ def create_repo(nickname, reponame, desc):
     result = Repository.ver_repeat(reponame, nickname)
     res = class2data(result, ["reponame"])
     if not res:
-        repoRepo.init(os.path.join(GIT_ROOT, nickname, reponame), bare=True)
+        repo = Repo.init(os.path.join(GIT_ROOT, nickname, reponame), bare=True)
        
         result = Repository.create_repo(reponame, nickname, desc)
     else:
@@ -25,12 +26,26 @@ def create_repo(nickname, reponame, desc):
     ret["msg"] = result    
     return ret 
 
+def clone_bare(bare_path, cloned_path):
+    repo = Repo(bare_path)
+    if os.path.isdir(cloned_path):      
+        cloned_mtime = os.stat(cloned_path).st_mtime
+        objects_mtime = os.stat(os.path.join(bare_path, "objects")).st_mtime
+        if objects_mtime > cloned_mtime:
+            shutil.rmtree(cloned_path) 
+            repo.clone(cloned_path)
+    else:
+        repo.clone(cloned_path)
+
 def get_data_from_directory(nickname, reponame, path_from_url):
-    file_path = os.path.join(GIT_ROOT, nickname, reponame, path_from_url)
-    
+    bare_path = os.path.join(GIT_ROOT, nickname, reponame)
+    cloned_path = os.path.join(bare_path, reponame)
+    clone_bare(bare_path, cloned_path) 
+    file_path = os.path.join(cloned_path, path_from_url)
+
     if os.path.isdir(file_path):
         files = [entry.name for entry in os.scandir(file_path) if not entry.is_dir()]
-        dirs = [entry.name for entry in os.scandir(file_path) if entry.is_dir()]
+        dirs = [entry.name for entry in os.scandir(file_path) if entry.is_dir() and entry.name != '.git']
         return create_response(0, "success", files=files, directories=dirs)
     elif os.path.isfile(file_path):
         with open(file_path) as f:
